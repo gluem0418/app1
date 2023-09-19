a0b21729ce0fb7a35b44320aa112874b2fc87273
gluem <dayes3010@gmail.com>
2023-09-19 14:19:52 +0900

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'app1', //追加
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
